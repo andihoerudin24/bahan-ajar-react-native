afa7715777882065ca08bf5146a48ad7d21bf4c0
AndiHoerudin <andihoerudin24@gmail.com>
2020-10-20 13:36:52 +0700

    Update dummy-data.js

--- dummy-data.js
+++ dummy-data.js
@@ -22,52 +22,6 @@ export const FAVORITE = [
   new Favorite('3','5'),
 ];
 
-export const RECEP =[
-  new Recep(
-    '1',
-    '3',
-    'simple',
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg/800px-Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg',
-    20,
-    [
-      '4 Tomatoes',
-      '1 Tablespoon of Olive Oil',
-      '1 Onion',
-      '250g Spaghetti',
-      'Spices',
-      'Cheese (optional)'
-    ],
-    [
-      'Cut the tomatoes and the onion into small pieces.',
-      'Boil some water - add salt to it once it boils.',
-      'Put the spaghetti into the boiling water - they should be done in about 10 to 12 minutes.',
-      'In the meantime, heaten up some olive oil and add the cut onion.',
-      'After 2 minutes, add the tomato pieces, salt, pepper and your other spices.',
-      'The sauce will be done once the spaghetti are.',
-      'Feel free to add some cheese on top of the finished dish.'
-    ],
-  ),
-  new Recep(
-    '1',
-    '10',
-    'simple',
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg/800px-Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg',
-    15,
-    [
-      '1 Slice White Bread',
-      '1 Slice Ham',
-      '1 Slice Pineapple',
-      '1-2 Slices of Cheese',
-      'Butter'
-    ],
-    [
-      'Butter one side of the white bread',
-      'Layer ham, the pineapple and cheese on the white bread',
-      'Bake the toast for round about 10 minutes in the oven at 200°C'
-    ],
-  )
-]
-
 
 export const RECEP =[
   new Recep(
@@ -114,3 +68,4 @@ export const RECEP =[
     ],
   )
 ]
+
